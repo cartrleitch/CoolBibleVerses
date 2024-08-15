696bf9411512d6eefc95beb76330767f04404e86
Carter Leitch <csleitch@students.unwsp.edu>
2024-08-14 20:01:58 -0500

    Add project files.

---
+++ CoolBibleVerses/Models/BibleVerse.cs
@@ -0,0 +1,13 @@
+﻿namespace CoolBibleVerses.Models
+{
+    public class BibleVerse
+    {
+        public int Id { get; set; }
+        public string Book { get; set; }
+        public int Chapter { get; set; }
+        public int Verse { get; set; }
+        public string Text { get; set; }
+
+        // maybe ESV API instead of Text stored in DB
+    }
+}
