23bcc828c16f0e234322d54f8695cd6a5d8dc892
Carter Leitch <csleitch@students.unwsp.edu>
2024-08-17 19:37:28 -0500

    Allowed searching by details and book-chapter-verse; need to allow searching by tags, and also need to order results be relevancy (put more weight on Book-Chapter-Verse than text or something); order by Genesis to Revelation by default on index and searches by text.

--- CoolBibleVerses/Controllers/BibleVersesController.cs
+++ CoolBibleVerses/Controllers/BibleVersesController.cs
@@ -160,7 +160,10 @@ namespace CoolBibleVerses.Controllers
 
         public async Task<IActionResult> ShowSearchResults(String SearchTerm)
         {
-            return View("Index", await _context.BibleVerse.Where(v => v.Text.Contains(SearchTerm) || v.Details.Contains(SearchTerm)).ToListAsync());
+            string searchTerm = SearchTerm.ToLower();
+            return View("Index", await _context.BibleVerse.Where(v => v.Text.ToLower().Contains(searchTerm) || v.Details.ToLower().Contains(searchTerm) 
+            || (searchTerm.Contains(v.Book.ToLower()) && searchTerm.Contains(v.Chapter.ToString().ToLower()) && searchTerm.Contains(v.Verse.ToString().ToLower())))
+                .ToListAsync());
         }
 
         private bool BibleVerseExists(int id)
