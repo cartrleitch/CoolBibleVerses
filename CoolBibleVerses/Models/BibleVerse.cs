074a57cf7b5b3dc14bcd00144a48a47b61d3d2c5
Carter Leitch <csleitch@students.unwsp.edu>
2024-08-26 16:00:15 -0500

    Added ability to create BibleVerse entry with just chapter, one verse, or multiple verses

--- CoolBibleVerses/Models/BibleVerse.cs
+++ CoolBibleVerses/Models/BibleVerse.cs
@@ -1,10 +1,13 @@
-﻿namespace CoolBibleVerses.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoolBibleVerses.Models
 {
     public class BibleVerse
     {
         public int Id { get; set; }
         public int BibleBookId { get; set; }
         public int Chapter { get; set; }
+        [Display(Name = "Verse(s)")]
         public int? Verse { get; set; }
         public int? VerseEnd { get; set; }
         public string? Text { get; set; }
